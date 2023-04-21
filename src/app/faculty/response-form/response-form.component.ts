682003bc8fa6faa63d36bf26c282f475841b3bcb
Tintu K John <tintukjohn@gmail.com>
2023-04-22 03:47:40 +0800

    updated

--- src/app/faculty/response-form/response-form.component.ts
+++ src/app/faculty/response-form/response-form.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-response-form',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./response-form.component.css']
 })
 export class ResponseFormComponent {
+
+  constructor(private router:Router,private api:ApiService,
+    private fb:FormBuilder,private route:ActivatedRoute){}
+
+  responseForm = this.fb.group(
+    {
+      Comment: ['', Validators.required],
+
+      file: ['', Validators.required]
+    })
+
+
+  onFileSelect(event: any){
+
+  }
+
+  onSubmit(){
+
+  }
 
 }
